1fd52e346a339963a9877d4f1d95421a32382a8c
Jackson Alves de Sá <Jackson.sa205@gmail.com>
2025-01-25 13:48:41 -0300

    [FIX] criado build para arquivos estaticos

--- app/backend/app.js
+++ app/backend/app.js
@@ -1,17 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); // Import para lidar com caminhos
 const { cadastro } = require('./src/routes/index.routes');
 
 const app = express();
-
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Rotas
+// Servir os arquivos estáticos do frontend
+const frontendPath = path.join(__dirname, '../frontend/build');
+app.use(express.static(frontendPath));
+
+// Rotas de API
 app.use('/usuario', cadastro);
+
+// Rota para servir o frontend
+app.get('*', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
+});
 
 // Porta do Servidor
 const PORT = process.env.PORT || 5000;
